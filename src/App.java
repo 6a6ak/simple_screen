04a6e6cd2ca4ffa4759fa358d87632e3fc06decd
6a6ak <babak.ahari@gmail.com>
2024-09-24 14:19:07 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) {
+        // Create a new JFrame object
+        JFrame frame = new JFrame("Simple GUI");
+
+        // Set the size of the frame (300x600)
+        frame.setSize(300, 600);
+
+        // Set default close operation to exit the program when the window is closed
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Make the frame visible
+        frame.setVisible(true);
+    }
+}
